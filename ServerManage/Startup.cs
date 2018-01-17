afa94d04f4a4d509f326f24edf7488592e6500b3
augustinus ray <augustinuszx@outlook.com>
2018-01-17 08:33:06 +0800

    添加项目文件。

---
+++ ServerManage/Startup.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ServerManage
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                //开发环境异常处理
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //生产环境异常处理
+                app.UseExceptionHandler("/Error");
+            }
+            //使用静态文件
+            app.UseStaticFiles();
+            //使用Mvc，设置默认路由
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                       name: "default",
+                       template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
